9a21fc8cd4f19701618d1f11173e5b57ea94eb83
Erik Illaste <erik.illaste@gmail.com>
2018-09-21 11:59:28 +0300

    third

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -13,7 +13,7 @@ exports.bookinstance_list = function(req, res, next) {
         // Successful, so render
         res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
       });
-      
+      //lol
   };
 
 // Display detail page for a specific BookInstance.
